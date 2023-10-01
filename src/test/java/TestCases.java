85ed5e9d925c53cba791baef707fbbe81be9df73
bertdriessen <bert.driessen@gmail.com>
2023-10-01 20:39:10 +0200

    Initial commit

---
+++ src/test/java/TestCases.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+    @Test
+    public void test1(){
+        System.out.println("enter test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("enter test 2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("enter test 1");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("enter test 1");
+    }
+}
